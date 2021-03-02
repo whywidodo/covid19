9b05d7ca42cc8f422787ddf8dd2c75e85cf4c5c6
whywidodo <mr.wahyuwidodo@gmail.com>
2021-03-02 16:28:57 +0700

    update time format

--- script.js
+++ script.js
@@ -22,11 +22,12 @@ $.ajax({
     type: 'get',
     dataType: 'json',
     success: function (detail) {
-        //console.log(data);
+        let date = moment(detail['lastUpdate']);
+        let lastUpdate = date.utcOffset('+0700').format('DD MMMM YYYY HH:mm')
         $('.positif').html(new Intl.NumberFormat().format(detail['confirmed']['value']));
         $('.sembuh').html(new Intl.NumberFormat().format(detail['recovered']['value']));
         $('.meninggal').html(new Intl.NumberFormat().format(detail['deaths']['value']));
-        $('.update-data').html(detail['lastUpdate']);
+        $('.update-data').html(lastUpdate + ' WIB');
     }
 });
 
