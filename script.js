04dab7477e90ccdb29e164b710d97937186e8692
whywidodo <mr.wahyuwidodo@gmail.com>
2021-03-02 16:43:50 +0700

    update negara

--- script.js
+++ script.js
@@ -16,14 +16,29 @@
 //     }
 // });
 
-//API Mathdroid
+//API Mathdroid Dunia
+// $.ajax({
+//     url: 'https://covid19.mathdro.id/api/',
+//     type: 'get',
+//     dataType: 'json',
+//     success: function (detail) {
+//         let date = moment(detail['lastUpdate']);
+//         let lastUpdate = date.utcOffset('+0700').format('DD MMMM YYYY HH:mm')
+//         $('.positif').html(new Intl.NumberFormat().format(detail['confirmed']['value']));
+//         $('.sembuh').html(new Intl.NumberFormat().format(detail['recovered']['value']));
+//         $('.meninggal').html(new Intl.NumberFormat().format(detail['deaths']['value']));
+//         $('.update-data').html(lastUpdate + ' WIB');
+//     }
+// });
+
+//API Mathdroid Indonesia
 $.ajax({
-    url: 'https://covid19.mathdro.id/api/',
+    url: 'https://covid19.mathdro.id/api/countries/Indonesia',
     type: 'get',
     dataType: 'json',
     success: function (detail) {
         let date = moment(detail['lastUpdate']);
-        let lastUpdate = date.utcOffset('+0700').format('DD MMMM YYYY HH:mm')
+        let lastUpdate = date.utcOffset('+0700').format('DD MMMM YYYY HH:mm');
         $('.positif').html(new Intl.NumberFormat().format(detail['confirmed']['value']));
         $('.sembuh').html(new Intl.NumberFormat().format(detail['recovered']['value']));
         $('.meninggal').html(new Intl.NumberFormat().format(detail['deaths']['value']));
